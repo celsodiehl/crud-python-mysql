e86675245912848e44dc94466534f7e331c6e374
Celso <celsodiehl@gmail.com>
2025-08-15 17:58:11 -0300

    Criado arquivos create.HTML e cadastrado no banco

--- app.py
+++ app.py
@@ -16,16 +16,42 @@ mysql.init_app(app)
 
 def home():
 
-    sql = ""
+    sql = "INSERT INTO `empregados` (`id`, `nome`, `email`, `foto`) VALUES (NULL, 'Amanda', 'amanda@gmail.com', 'fotoamanda.jpg'); "
     conn = mysql.connect()
     cursor=conn.cursor()
     cursor.execute(sql)
     conn.commit()
+    return render_template('funcionarios/index.html')
 
-    
+#Essa rota chama o template Create.HTML
+@app.route('/create')
+def create():
+    return render_template('funcionarios/create.html')
+
     if request.method=='POST':
         # Handle POST Request here
         return render_template('index.html')
+    return render_template('funcionarios/index.html')
+
+#Rota para enviar dados do create action = store
+@app.route('/store', methods=['POST'])
+def storage():
+
+    #variaveis:
+    _nome = request.form['txtNome']
+    _email = request.form['txtEmail']
+    _foto = request.files['txtFoto']
+                                                                                #vai enviar nessa ordem
+    sql = "INSERT INTO `empregados` (`id`, `nome`, `email`, `foto`) VALUES (NULL, %s, %s, %s);"
+    
+    #na foto pego o filename por que tem outros parametros dela, por ex: tamanho e tipo JPG...
+    #vai enviar nessa ordem
+    dados =(_nome, _email, _foto.filename)
+
+    conn = mysql.connect()
+    cursor=conn.cursor()
+    cursor.execute(sql, dados)
+    conn.commit()
     return render_template('funcionarios/index.html')
 
 if __name__ == '__main__':
